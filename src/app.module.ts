d675457eb582e3aa7444da4241617ccf4b9b2c68
karthick <karthickkumar7.k7@gmail.com>
2024-09-16 04:08:24 -0700

    initailized

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      entities: [User, Report],
+      database: 'db.sqlite',
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
